b131ec1d6f247513193e218037380984ee33ab9b
Tyler Porter <tyler.b.porter@gmail.com>
2026-07-01 14:46:30 -0400

    Rename crystals

--- internal/alttp/logic/prizes.go
+++ internal/alttp/logic/prizes.go
@@ -1,12 +1,12 @@
 package logic
 
-var crystalNames = []string{
+var crystals = []string{
 	"Crystal 1", "Crystal 2", "Crystal 3", "Crystal 4", "Crystal 5", "Crystal 6", "Crystal 7",
 }
 
 func (i *Items) CrystalCount() int {
 	count := 0
-	for _, name := range crystalNames {
+	for _, name := range crystals {
 		if i.Has(name) {
 			count++
 		}
