496074a89a5b71c98e36efe9c6f7fc2bc720255b
Tyler Porter <tyler.b.porter@gmail.com>
2026-07-02 11:17:36 -0400

    Do not emit graph data, update CLI

---
+++ internal/alttp/solve.go
@@ -0,0 +1,22 @@
+package alttp
+
+import (
+	"github.com/alttpr-mudora/mudora/internal/alttp/graph"
+	"github.com/alttpr-mudora/mudora/internal/alttp/logic"
+	"github.com/alttpr-mudora/mudora/internal/rom"
+)
+
+func Solve(data []byte) *graph.Graph {
+	itemAt := make(map[string]string)
+	for _, e := range rom.Inspect(data) {
+		itemAt[e.Location] = e.Item
+	}
+
+	settings := logic.DefaultSettings()
+	settings.LocationItems = itemAt
+	medallions := rom.RequiredMedallions(data)
+	settings.MiseryMireMedallion = medallions["Misery Mire"]
+	settings.TurtleRockMedallion = medallions["Turtle Rock"]
+
+	return graph.MinimalPathTo(itemAt, settings, "Ganons Tower")
+}
